50a6a6a096b5e85d4b5905b368b6d50be9bc0d95
Snehanjani Pati <157661040+SnehanjaniPati@users.noreply.github.com>
2025-05-07 10:24:27 -0400

    Delete Frontend directory

--- Frontend/logout.js
+++
@@ -1,9 +0,0 @@
-app.post('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return res.redirect('/dashboard');
-        }
-        res.clearCookie('sessionId');
-        res.redirect('/logged-out');
-    });
-});
